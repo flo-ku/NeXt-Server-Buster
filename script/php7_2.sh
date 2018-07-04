aa0b8b987b8f28c16c01a8299333bf1a7ab49165
Retender <retender.jw@gmail.com>
2018-07-04 16:41:21 +0200

    -

--- script/php7_2.sh
+++ script/php7_2.sh
@@ -13,8 +13,8 @@ apt-get update -y >/dev/null 2>&1
 
 ###workaround
 install_packages "libcurl3"
-wget http://security.debian.org/debian-security/pool/updates/main/i/icu/libicu57_57.1-6+deb9u2_amd64.deb
-dpkg -i libicu57_57.1-6+deb9u2_amd64.deb
+wget_tar "http://security.debian.org/debian-security/pool/updates/main/i/icu/libicu57_57.1-6+deb9u2_amd64.deb"
+dpkg -i libicu57_57.1-6+deb9u2_amd64.deb >>"${main_log}" 2>>"${err_log}"
 
 PHPVERSION7="7.2"
 
