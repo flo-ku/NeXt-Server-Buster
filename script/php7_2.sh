39e9b483c571bb9b6f5a384e0f221a5681004d5b
Retender <retender.jw@gmail.com>
2018-07-08 01:03:06 +0200

    -

--- script/php7_2.sh
+++ script/php7_2.sh
@@ -5,19 +5,9 @@
 
 install_php_7_2() {
 
-install_packages "apt-transport-https"
-wget --no-check-certificate -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg >>"${main_log}" 2>>"${err_log}"
-echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list
-
-apt-get update -y >/dev/null 2>&1
-
 PHPVERSION7="7.2"
 
 install_packages "php-auth-sasl php-http-request php$PHPVERSION7-gd php$PHPVERSION7-bcmath php$PHPVERSION7-zip php-mail php-net-dime php-net-url php-pear php-apcu php$PHPVERSION7 php$PHPVERSION7-cli php$PHPVERSION7-common php$PHPVERSION7-curl php$PHPVERSION7-dev php$PHPVERSION7-fpm php$PHPVERSION7-mysql php$PHPVERSION7-soap php$PHPVERSION7-sqlite3 php$PHPVERSION7-xsl php$PHPVERSION7-xmlrpc php-mbstring php-xml php$PHPVERSION7-json php$PHPVERSION7-opcache php$PHPVERSION7-readline php$PHPVERSION7-xml php$PHPVERSION7-mbstring php-memcached"
-
-#php$PHPVERSION7-intl
-#wget_tar "http://ftp.de.debian.org/debian/pool/main/p/php7.2/php7.2-intl_7.2.4-1+b2_amd64.deb"
-#dpkg -i php7.2-intl_7.2.4-1+b2_amd64.deb >>"${main_log}" 2>>"${err_log}"
 
 cp ${SCRIPT_PATH}/configs/php/php.ini /etc/php/$PHPVERSION7/fpm/php.ini
 cp ${SCRIPT_PATH}/configs/php/php-fpm.conf /etc/php/$PHPVERSION7/fpm/php-fpm.conf
