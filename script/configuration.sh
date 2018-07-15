bb1a9e77782607fa51c92e8fcc83db5bf271ebc8
Retender <retender.jw@gmail.com>
2018-07-15 13:45:43 +0200

    Update configuration.sh

--- script/configuration.sh
+++ script/configuration.sh
@@ -7,13 +7,14 @@ start_after_install() {
 
   source ${SCRIPT_PATH}/configs/userconfig.cfg
   source ${SCRIPT_PATH}/script/functions.sh
+  source ${SCRIPT_PATH}/configs/versions.cfg
+
   greenb() { echo $(tput bold)$(tput setaf 2)${1}$(tput sgr0); }
   ok="$(greenb [OKAY] -)"
   redb() { echo $(tput bold)$(tput setaf 1)${1}$(tput sgr0); }
   error="$(redb [ERROR] -)"
 
   source ${SCRIPT_PATH}/checks/nginx-check.sh; check_nginx
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -21,7 +22,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/php-check.sh; check_php
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -29,7 +29,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/openssh-check.sh; check_openssh
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -37,7 +36,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/fail2ban-check.sh; check_fail2ban
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -45,7 +43,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/unbound-check.sh; check_unbound
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -53,7 +50,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/dovecot-check.sh; check_dovecot
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -61,7 +57,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/postfix-check.sh; check_postfix
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -69,7 +64,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/rspamd-check.sh; check_rspamd
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -77,7 +71,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/lets_encrypt-check.sh; check_lets_encrypt
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -85,7 +78,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/firewall-check.sh; check_firewall
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -93,7 +85,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/mailman-check.sh; check_mailman
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -101,7 +92,6 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/mailserver-check.sh; check_mailserver
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
@@ -109,66 +99,41 @@ start_after_install() {
   fi
 
   source ${SCRIPT_PATH}/checks/system-check.sh; check_system
-  #dialog_yesno_configuration
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
   exit 1
   fi
 
-  source ${SCRIPT_PATH}/configs/versions.cfg
-	source ${SCRIPT_PATH}/script/configuration.sh; show_ssh_key
+	source ${SCRIPT_PATH}/script/openssh-options.sh; show_ssh_key
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
   exit 1
 
-	source ${SCRIPT_PATH}/script/configuration.sh; show_login_information.txt
+  dialog_msg "Please save the shown login information on next page"
+  cat ${SCRIPT_PATH}/login_information.txt
   read -p "Continue (y/n)?" ANSW
   if [ "$ANSW" = "n" ]; then
   echo "Exit"
   exit 1
 
-	source ${SCRIPT_PATH}/script/configuration.sh; create_private_key
+	source ${SCRIPT_PATH}/script/openssh-options.sh.sh; create_private_key
+  read -p "Continue (y/n)?" ANSW
+  if [ "$ANSW" = "n" ]; then
+  echo "Exit"
+  exit 1
 
   if [[ ${USE_MAILSERVER} = "1" ]]; then
-  read -p "Continue (y/n)?" ANSW
-  if [ "$ANSW" = "n" ]; then
-  echo "Exit"
-  exit 1
   source ${SCRIPT_PATH}/script/configuration.sh; show_dkim_key
   fi
 
-	dialog --backtitle "NeXt Server Installation" --msgbox "Finished after installation configuration" $HEIGHT $WIDTH
-}
-
-show_ssh_key() {
-dialog --backtitle "NeXt Server Configuration" --msgbox "Please save the shown SSH privatekey on next page into a textfile on your PC. \n\n
-Important: \n
-In Putty you have only mark the text. Do not Press STRG+C!" $HEIGHT $WIDTH
-#dialog --title "Your SSH Privatekey" --tab-correct --exit-label "ok" --textbox ${SCRIPT_PATH}/ssh_privatekey.txt 50 200
-cat ${SCRIPT_PATH}/ssh_privatekey.txt
-}
-
-show_login_information.txt() {
-dialog_msg "Please save the shown login information on next page"
-#dialog --title "Your Server Logininformations" --tab-correct --exit-label "ok" --textbox ${SCRIPT_PATH}/login_information.txt 50 200
-cat ${SCRIPT_PATH}/login_information.txt
-}
-
-create_private_key() {
-dialog --backtitle "NeXt Server Configuration" --msgbox "You have to download the latest PuTTYgen \n (https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html) \n \n
-Start the program and click on Conversions- Import key. \n
-Now select the Text file, where you saved the ssh_privatekey. \n
-After entering your SSH Password, you have to switch the paramter from RSA to ED25519. \n
-In the last step click on save private key - done! \n \n
-Dont forget to change your SSH Port in PuTTY!" $HEIGHT $WIDTH
+  dialog_msg "Finished after installation configuration"
 }
 
 show_dkim_key() {
 dialog --backtitle "NeXt Server Configuration" --msgbox "Please enter the shown DKIM key on next page to you DNS settings \n\n
 remove all quote signs - so it looks like that:  \n\n
-v=DKIM1; k=rsa; p=MIIBIjANBgkqh[...] "$HEIGHT $WIDTH
-#dialog --title "Your Server Logininformations" --tab-correct  --exit-label "ok"--textbox ${SCRIPT_PATH}/DKIM_KEY_ADD_TO_DNS.txt 50 200
+v=DKIM1; k=rsa; p=MIIBIjANBgkqh[...] " $HEIGHT $WIDTH
 cat ${SCRIPT_PATH}/DKIM_KEY_ADD_TO_DNS.txt
 }
