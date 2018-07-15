697f9d93da5d8cd0bb2c9308ce41b6a6018e9a96
Retender <retender.jw@gmail.com>
2018-07-15 13:45:59 +0200

    Update openssh_options.sh

--- script/openssh_options.sh
+++ script/openssh_options.sh
@@ -42,3 +42,19 @@ mv ~/ssh.key ${SCRIPT_PATH}/ssh_privatekey.txt
 
 service sshd restart
 }
+
+show_ssh_key() {
+dialog --backtitle "NeXt Server Configuration" --msgbox "Please save the shown SSH privatekey on next page into a textfile on your PC. \n\n
+Important: \n
+In Putty you have only mark the text. Do not Press STRG+C!" $HEIGHT $WIDTH
+cat ${SCRIPT_PATH}/ssh_privatekey.txt
+}
+
+create_private_key() {
+dialog --backtitle "NeXt Server Configuration" --msgbox "You have to download the latest PuTTYgen \n (https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html) \n \n
+Start the program and click on Conversions- Import key. \n
+Now select the Text file, where you saved the ssh_privatekey. \n
+After entering your SSH Password, you have to switch the paramter from RSA to ED25519. \n
+In the last step click on save private key - done! \n \n
+Dont forget to change your SSH Port in PuTTY!" $HEIGHT $WIDTH
+}
