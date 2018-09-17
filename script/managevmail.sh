25ff1357de5b790983788c48705de1a16cc4a544
shoujii <retender.jw@gmail.com>
2018-09-17 21:28:20 +0200

    -

---
+++ script/managevmail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Compatible with Debian 10.x Buster
+#Please check the license provided with the script!
+#-------------------------------------------------------------------------------------------------------------
+
+
+install_managevmail() {
+
+install_packages "python3 python3-mysql.connector"
+
+}
