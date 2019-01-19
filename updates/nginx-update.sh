e079dca8efbb83a72230f03608d55133bfef3d25
shoujii <retender.jw@gmail.com>
2019-01-19 20:05:08 +0100

    -

--- updates/nginx-update.sh
+++ updates/nginx-update.sh
@@ -34,21 +34,6 @@ if [[ ${LOCAL_NGINX_VERSION} != ${NGINX_VERSION} ]]; then
 
   wget_tar "https://dl.google.com/dl/page-speed/psol/${PSOL_VERSION}-x64.tar.gz"
   tar_file "${PSOL_VERSION}-x64.tar.gz"
-  cd ${SCRIPT_PATH}/sources
-  git clone --recursive https://github.com/bagder/libbrotli >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to clone libbrotli"
-  cd libbrotli
-  autoreconf -v -i >>"${main_log}" 2>>"${err_log}"
-  ./autogen.sh >>"${main_log}" 2>>"${err_log}"
-  ./configure >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to configure libbrotli"
-  mkdir brotli/c/tools/.deps && touch brotli/c/tools/.deps/brotli-brotli.Po
-  make -j $(nproc) >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to make libbrotli"
-  make install >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to install libbrotli"
-  ldconfig
-
-  cd ${SCRIPT_PATH}/sources
-  git clone https://github.com/google/ngx_brotli >>"${main_log}" 2>>"${err_log}" || error_exit "Failed to clone ngx_brotli"
-  cd ngx_brotli
-  git submodule update --init >>"${main_log}" 2>>"${err_log}"
 
   cd ${SCRIPT_PATH}/sources
   wget_tar "https://codeload.github.com/openresty/headers-more-nginx-module/zip/v${NGINX_HEADER_MOD_VERSION}"
