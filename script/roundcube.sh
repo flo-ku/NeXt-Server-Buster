07b4bc522dc1c3e39266b2d82bc11aa2e1fd4bf0
shoujii <retender.jw@gmail.com>
2019-01-26 16:45:56 +0100

    -

--- script/roundcube.sh
+++ script/roundcube.sh
@@ -24,12 +24,12 @@ MYSQL_ROOT_PASS=$(grep -Pom 1 "(?<=^MYSQL_ROOT_PASS: ).*$" ${SCRIPT_PATH}/login_
 ROUNDCUBE_USER=$(username)
 ROUNDCUBE_DB_PASS=$(password)
 ROUNDCUBE_DB_NAME=$(username)
+DES_KEY=$(username)
 
 mysql -u root -p${MYSQL_ROOT_PASS} -e "CREATE DATABASE ${ROUNDCUBE_DB_NAME};"
 mysql -u root -p${MYSQL_ROOT_PASS} -e "CREATE USER '${ROUNDCUBE_USER}'@'localhost' IDENTIFIED BY '${ROUNDCUBE_DB_PASS}';"
 mysql -u root -p${MYSQL_ROOT_PASS} -e "GRANT ALL PRIVILEGES ON ${ROUNDCUBE_DB_NAME}.* TO '${ROUNDCUBE_USER}'@'localhost';"
 mysql -u root -p${MYSQL_ROOT_PASS} -e "FLUSH PRIVILEGES;"
-
 mysql -u root -p"${MYSQL_ROOT_PASS}" "${ROUNDCUBE_DB_NAME}" < /var/www/${MYDOMAIN}/public/webmail/SQL/mysql.initial.sql
 
 cp /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php.sample /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
@@ -38,9 +38,7 @@ sed -i "s|^\(\$config\['db_dsnw'\] =\).*$|\1 \'mysqli://roundcube:${ROUNDCUBE_DB
 sed -i "s|^\(\$config\['smtp_server'\] =\).*$|\1 \'localhost\';|" /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
 sed -i "s|^\(\$config\['smtp_user'\] =\).*$|\1 \'%u\';|" /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
 sed -i "s|^\(\$config\['smtp_pass'\] =\).*$|\1 \'%p\';|" /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
-
-deskey=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9-_#&!*%?' | fold -w 24 | head -n 1)
-sed -i "s|^\(\$config\['des_key'\] =\).*$|\1 \'${deskey}\';|" /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
+sed -i "s|^\(\$config\['des_key'\] =\).*$|\1 \'${DES_KEY}\';|" /var/www/${MYDOMAIN}/public/webmail/config/config.inc.php
 
 rm -rf /var/www/html/roundcube/installer
 
