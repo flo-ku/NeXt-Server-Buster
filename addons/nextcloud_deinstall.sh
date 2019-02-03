2f80d018104258a8d8ecc859270de7c6b0729ad5
Marcel Goessel <info@mgoessel.de>
2019-02-03 16:37:10 +0100

    -

--- addons/nextcloud_deinstall.sh
+++ addons/nextcloud_deinstall.sh
@@ -13,7 +13,7 @@ NextcloudDBName=$(grep -Pom 1 "(?<=^NextcloudDBName = ).*$" ${SCRIPT_PATH}/nextc
 NextcloudDBUser=$(grep -Pom 1 "(?<=^NextcloudDBUser = ).*$" ${SCRIPT_PATH}/nextcloud_login_data.txt)
 
 mysql -u root -p${MYSQL_ROOT_PASS} -e "DROP DATABASE IF EXISTS ${NextcloudDBName};"
-mysql -u root -p${MYSQL_ROOT_PASS} -e "DROP USER ${NextcloudDBName}@localhost;"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "DROP USER ${NextcloudDBUser}@localhost;"
 
 rm -rf /var/www/${MYDOMAIN}/public/${NEXTCLOUD_PATH_NAME}
 rm ${SCRIPT_PATH}/nextcloud_login_data.txt
