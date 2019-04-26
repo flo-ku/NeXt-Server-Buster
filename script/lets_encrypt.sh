f825b12ad2308100850f09b46926a95fb7928155
Marcel Goessel <info@mgoessel.de>
2019-04-26 21:06:03 +0200

    -

--- script/lets_encrypt.sh
+++ script/lets_encrypt.sh
@@ -44,8 +44,7 @@ sed -i 's/HPKP1="1"/HPKP1="${HPKP1}"/' ${SCRIPT_PATH}/configs/userconfig.cfg
 sed -i 's/HPKP2="2"/HPKP2="${HPKP2}"/' ${SCRIPT_PATH}/configs/userconfig.cfg
 
 ### change path --> see system.sh
-## add function to main menu for updating cert
-echo "0 0 1 */3 *"   root    bash /root/test1234.sh >> /etc/crontab
+echo "0 0 1 */3 *"   root    bash /etc/cron.d/le_cert_alert >> /etc/crontab
 }
 
 update_nginx_cert() {
@@ -58,17 +57,16 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 systemctl -q stop nginx.service
 
 #cleanup old cert / key, maybe backup until success?
-rm /root/.acme.sh/${MYDOMAIN}_ecc/fullchain.cer
-rm /root/.acme.sh/${MYDOMAIN}_ecc/${MYDOMAIN}.key
+rm -R /root/.acme.sh/${MYDOMAIN}_ecc/
 rm /etc/nginx/ssl/${MYDOMAIN}-ecc.cer
 rm /etc/nginx/ssl/${MYDOMAIN}-ecc.key
 
 #delete old keys
-sed -i 's/HPKP1="${HPKP1}"/HPKP1="1"/' ${SCRIPT_PATH}/configs/userconfig.cfg
-sed -i 's/HPKP2="${HPKP2}"/HPKP2="2"/' ${SCRIPT_PATH}/configs/userconfig.cfg
+sed -i 's/HPKP1="'${HPKP1}'"/HPKP1="1"/' ${SCRIPT_PATH}/configs/userconfig.cfg
+sed -i 's/HPKP2="'${HPKP2}'"/HPKP2="2"/' ${SCRIPT_PATH}/configs/userconfig.cfg
 
 cd ${SCRIPT_PATH}/sources/acme.sh/
-bash acme.sh --issue --standalone --debug 2 --log -d ${MYDOMAIN} -d www.${MYDOMAIN} --keylength ec-384 --force-renew --staging >>"${main_log}" 2>>"${err_log}"
+bash acme.sh --issue --standalone --debug 2 --log -d ${MYDOMAIN} -d www.${MYDOMAIN} --keylength ec-384 --staging >>"${main_log}" 2>>"${err_log}"
 
 ln -s /root/.acme.sh/${MYDOMAIN}_ecc/fullchain.cer /etc/nginx/ssl/${MYDOMAIN}-ecc.cer >>"${main_log}" 2>>"${err_log}"
 ln -s /root/.acme.sh/${MYDOMAIN}_ecc/${MYDOMAIN}.key /etc/nginx/ssl/${MYDOMAIN}-ecc.key >>"${main_log}" 2>>"${err_log}"
@@ -83,11 +81,8 @@ HPKP2=$(echo "$HPKP2" | sed 's/\//\\\//g')
 sed -i "s/HPKP1/${HPKP1}/g" /etc/nginx/_general.conf
 sed -i "s/HPKP2/${HPKP2}/g" /etc/nginx/_general.conf
 
-sed -i 's/HPKP1="1"/HPKP1="${HPKP1}"/' ${SCRIPT_PATH}/configs/userconfig.cfg
-sed -i 's/HPKP2="2"/HPKP2="${HPKP2}"/' ${SCRIPT_PATH}/configs/userconfig.cfg
-
-### change path --> see system.sh
-echo "0 0 1 */3 *"   root    bash /etc/cron.d/le_cert_alert >> /etc/crontab
+sed -i 's/HPKP1="1"/HPKP1="'${HPKP1}'"/' ${SCRIPT_PATH}/configs/userconfig.cfg
+sed -i 's/HPKP2="2"/HPKP2="'${HPKP2}'"/' ${SCRIPT_PATH}/configs/userconfig.cfg
 
 systemctl -q start nginx.service
 }
@@ -101,13 +96,12 @@ source ${SCRIPT_PATH}/configs/userconfig.cfg
 
 systemctl -q stop nginx.service
 
-rm /root/.acme.sh/mail.${MYDOMAIN}/fullchain.cer
-rm /root/.acme.sh/mail.${MYDOMAIN}/mail.${MYDOMAIN}.key
+rm -R /root/.acme.sh/$mail.${MYDOMAIN}
 rm /etc/nginx/ssl/mail.${MYDOMAIN}.cer
 rm /etc/nginx/ssl/mail.${MYDOMAIN}.key
 
 cd ${SCRIPT_PATH}/sources/acme.sh/
-bash acme.sh --issue --debug 2 --standalone -d mail.${MYDOMAIN} -d imap.${MYDOMAIN} -d smtp.${MYDOMAIN} --keylength 4096 --force-renew --staging >>"${main_log}" 2>>"${err_log}"
+bash acme.sh --issue --debug 2 --standalone -d mail.${MYDOMAIN} -d imap.${MYDOMAIN} -d smtp.${MYDOMAIN} --keylength 4096 --staging >>"${main_log}" 2>>"${err_log}"
 
 ln -s /root/.acme.sh/mail.${MYDOMAIN}/fullchain.cer /etc/nginx/ssl/mail.${MYDOMAIN}.cer
 ln -s /root/.acme.sh/mail.${MYDOMAIN}/mail.${MYDOMAIN}.key /etc/nginx/ssl/mail.${MYDOMAIN}.key
