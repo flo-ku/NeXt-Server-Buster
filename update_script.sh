001fdd0ec13733dbf325794d69b3ea7b88819ed5
Marcel Goessel <info@mgoessel.de>
2019-05-01 18:30:39 +0200

    -

--- update_script.sh
+++ update_script.sh
@@ -12,14 +12,16 @@ echo "Fetching from $remote..."
 git fetch $remote
 
 if git merge-base --is-ancestor $remote_branch HEAD; then
-    dialog_msg "Already up-to-date Version ${GIT_LOCAL_FILES_HEAD}"
+    remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+    dialog_msg "Already up-to-date Version ${remote_branch}"
     exit 0
 fi
 
 if git merge-base --is-ancestor HEAD $remote_branch; then
     git stash
     git merge --ff-only --stat $remote_branch
-    dialog_msg "Merged to the new Version ${GIT_LOCAL_FILES_HEAD}"
+    remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+    dialog_msg "Merged to the new Version ${remote_branch}"
 else
     echo 'Fast-forward not possible. Rebasing...'
     source ${SCRIPT_PATH}/script/functions.sh; continue_or_exit
