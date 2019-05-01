d63b0c97ac48da928bcc9b39156ccf1b8c86df29
Marcel Goessel <info@mgoessel.de>
2019-05-01 18:19:24 +0200

    -

--- update_script.sh
+++ update_script.sh
@@ -4,25 +4,23 @@
 
 update_script() {
 
-SCRIPT_PATH="/root/NeXt-Server-Buster" 
-cd ${SCRIPT_PATH}
+local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+remote=$(git config branch.$local_branch.remote)
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+echo "Fetching from $remote..."
+git fetch $remote
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    git reset --hard origin/master
-    dialog_msg "Finished updating NeXt Server Script to Version ${GIT_LOCAL_FILES_HEAD}"
+if git merge-base --is-ancestor $remote_branch HEAD; then
+    echo 'Already up-to-date'
+    exit 0
+fi
+
+if git merge-base --is-ancestor HEAD $remote_branch; then
+    echo 'Fast-forward possible. Merging...'
+    git merge --ff-only --stat $remote_branch
 else
-    echo "Diverged"
+    echo 'Fast-forward not possible. Rebasing...'
+    git rebase --preserve-merges --stat $remote_branch
 fi
 }
-
-
-
-
-          
