320fc269fd25999fedd6fdbcad5e33023b413de4
Florian <mykuenstler@gmail.com>
2019-07-18 15:42:58 +0200

    fixed rspamd; dkim public key was written  to main.log

--- script/rspamd.sh
+++ script/rspamd.sh
@@ -57,8 +57,7 @@ cp ${SCRIPT_PATH}/configs/rspamd/milter_headers.conf /etc/rspamd/local.d/milter_
 CURRENT_YEAR=$(date +'%Y')
 
 mkdir /var/lib/rspamd/dkim/
-rspamadm dkim_keygen -b 2048 -s ${CURRENT_YEAR} -k /var/lib/rspamd/dkim/${CURRENT_YEAR}.key >>"${main_log}" 2>>"${err_log}"
-cp /var/lib/rspamd/dkim/${CURRENT_YEAR}.key /var/lib/rspamd/dkim/${CURRENT_YEAR}.txt
+rspamadm dkim_keygen -b 2048 -s ${CURRENT_YEAR} -k /var/lib/rspamd/dkim/${CURRENT_YEAR}.key > /var/lib/rspamd/dkim/${CURRENT_YEAR}.txt
 chown -R _rspamd:_rspamd /var/lib/rspamd/dkim
 chmod 440 /var/lib/rspamd/dkim/*
 cp /var/lib/rspamd/dkim/${CURRENT_YEAR}.txt ${SCRIPT_PATH}/DKIM_KEY_ADD_TO_DNS.txt
